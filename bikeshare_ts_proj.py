58201948530c51b1467f36be30e382c9b0f91cf0
christopher-liu <christopherbrianliu@gmail.com>
2020-04-18 12:58:49 -0700

    Added code for plotting the H-W forecast

--- bikeshare_ts_proj.py
+++ bikeshare_ts_proj.py
@@ -34,16 +34,16 @@ bike_plot.legend(['Bike Rentals', 'H-W Model Fit'])
 # The MAPE (Mean Average Percentage Error) of the H-W model
 np.average(np.absolute((fit_hw.fittedvalues - bike['num_rides']) / bike['num_rides']))
 
+
 # Then for making forecasts with the H-W model
 fit_hw.forecast(12)
 
-# Plot the decomposition of the model's states/components
-states_decomp = pd.DataFrame(np.c_[fit_hw.level, fit_hw.slope, fit_hw.season], columns=['level','slope','seasonal'], index=bike.index)
-
-fig, [ax1, ax2, ax3] = plt.subplots(3, 1, figsize=(10,6))
-states_decomp[['level']].plot(ax = ax1, title = "Level")
-states_decomp[['slope']].plot(ax = ax2, title = "Trend/Slope")
-states_decomp[['seasonal']].plot(ax = ax3, title = "Seasonality")
+fig, ax = plt.subplots(figsize=(15, 6))
+fig = bike['num_rides'].plot(ax = ax, title = "Holt-Winters' Method Bike Share Forecast")
+fig.set_ylabel("Number of Bike rentals")
+fig.set_xlabel("Year")
+fit_hw.forecast(12).plot(ax = ax)
+fig.legend(['Bike Rentals', 'H-W Forecast']);
 
 
 
@@ -56,11 +56,13 @@ states_decomp[['seasonal']].plot(ax = ax3, title = "Seasonality")
 bike['seasonal_diff'] = bike.diff(periods = 12)
 bike['seasonal_diff'].plot(figsize = (10,6))
 
+
 # Plotting the original data alongside the seasonally differenced data
 fig, axes = plt.subplots(1, 2, figsize=(15, 6))
-bike['num_rides'].plot(ax = axes[0], title = "Monthly Bike Rentals")
-bike['seasonal_diff'].plot(ax = axes[1], title = "Monthly Bike Rentals - Seasonally Differenced")
+fig = bike['num_rides'].plot(ax = axes[0], title = "Monthly Bike Rentals")
+fig = bike['seasonal_diff'].plot(ax = axes[1], title = "Monthly Bike Rentals - Seasonally Differenced")
 axes[1].hlines(0, bike.index[0], bike.index[-1], 'r')
+
 
 # Plotting PACF and ACF plots of the seasonally differenced data
 fig, axes = plt.subplots(1, 2, figsize=(15,6))
