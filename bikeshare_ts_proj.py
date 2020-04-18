ae026952729aebaf41b902b0639d2333efdf0a10
christopher-liu <christopherbrianliu@gmail.com>
2020-04-18 13:46:38 -0700

    adding code for testing out an ARIMA(1,0,1)(1,1,0)12 model

--- bikeshare_ts_proj.py
+++ bikeshare_ts_proj.py
@@ -13,11 +13,11 @@ bike.set_index(pd.period_range('1/1/2013', freq='M', periods=84), inplace = True
 bike.head()
 
 # Plotting data along with ACF and PACF plots to see any seasonality/trend
-bike.plot(figsize = (10,6))
+bike['num_rides'].plot(figsize = (10,6))
 
 fig, axes = plt.subplots(1, 2, figsize=(15,6))
-fig = sm.graphics.tsa.plot_acf(bike, lags=48, ax=axes[0])
-fig = sm.graphics.tsa.plot_pacf(bike, lags=48, ax=axes[1])
+fig = sm.graphics.tsa.plot_acf(bike['num_rides'], lags=36, ax=axes[0])
+fig = sm.graphics.tsa.plot_pacf(bike['num_rides'], lags=36, ax=axes[1])
 
 
 # Fit a Holt-Winters model with additive trend and seasonality to our data
@@ -42,9 +42,8 @@ fig, ax = plt.subplots(figsize=(15, 6))
 fig = bike['num_rides'].plot(ax = ax, title = "Holt-Winters' Method Bike Share Forecast")
 fig.set_ylabel("Number of Bike rentals")
 fig.set_xlabel("Year")
-fit_hw.forecast(12).plot(ax = ax)
-fig.legend(['Bike Rentals', 'H-W Forecast']);
-
+fit_hw.forecast(12).plot(ax = ax,style = '--', color = 'DarkRed')
+fig.legend(['Bike Rentals', 'H-W Forecast'])
 
 
 
@@ -54,10 +53,7 @@ fig.legend(['Bike Rentals', 'H-W Forecast']);
 # Since we see clear seasonality in the data, we'll first try season differencing. The seasonally
 # differenced data looks much closer to being stationary
 bike['seasonal_diff'] = bike.diff(periods = 12)
-bike['seasonal_diff'].plot(figsize = (10,6))
 
-
-# Plotting the original data alongside the seasonally differenced data
 fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 fig = bike['num_rides'].plot(ax = axes[0], title = "Monthly Bike Rentals")
 fig = bike['seasonal_diff'].plot(ax = axes[1], title = "Monthly Bike Rentals - Seasonally Differenced")
@@ -70,5 +66,28 @@ fig = sm.graphics.tsa.plot_acf(bike['seasonal_diff'].iloc[12:], lags=36, ax=axes
 fig = sm.graphics.tsa.plot_pacf(bike['seasonal_diff'].iloc[12:], lags=36, ax=axes[1])
 
 
+fit_sarima = SARIMAX(bike['num_rides'], order = (1,0,1), seasonal_order=(1,1,0,12)).fit()
+
+fit_sarima.resid.plot()
 
 
+bike_plot = bike['num_rides'].plot(figsize = (10,6), title = "SARIMA Model Bike Share Fit")
+bike_plot.set_ylabel("Number of Bike rentals")
+bike_plot.set_xlabel("Year")
+
+fit_sarima.fittedvalues.plot(ax = bike_plot, style = '--', color = 'DarkOrange')
+bike_plot.legend(['Bike Rentals', 'SARIMA Model Fit'])
+
+
+
+
+
+# Plotting the SARIMA and H-W forecasts together. We see that the H-W model forecasts greater
+# values on average in comparison to the SARIMA
+fig, ax = plt.subplots(figsize=(15, 6))
+fig = bike['num_rides'].plot(ax = ax, title = "Holt-Winters' Method Bike Share Forecast")
+fig.set_ylabel("Number of Bike rentals")
+fig.set_xlabel("Year")
+fit_hw.forecast(12).plot(ax = ax,style = '--', color = 'DarkRed')
+fit_sarima.forecast(12).plot(ax = ax,style = '--', color = 'DarkOrange')
+fig.legend(['Bike Rentals', 'H-W Forecast', 'SARIMA Forecast'])
